5616fe1c34be56627f867927062f2ac8c4ca0627
Caio Willian <cwillian40@hotmail.com>
2022-07-03 19:16:01 -0300

    17. Implementando a Entidade Jogadores [ Parte 2 ]

--- src/jogadores/jogadores.controller.ts
+++ src/jogadores/jogadores.controller.ts
@@ -1,7 +1,8 @@
-import { Body, Controller, Post, Get } from '@nestjs/common';
+import { Body, Controller, Post, Get, Query, Delete } from '@nestjs/common';
 import { CriarJogadorDto } from './dtos/criar-jogador.dto';
 import { JogadoresService } from './jogadores.service';
 import { Jogador } from './interfaces/jogador.interface'
+import { query } from 'express';
 
 @Controller('api/v1/jogadores')
 export class JogadoresController {
@@ -9,13 +10,25 @@ export class JogadoresController {
   constructor(private readonly jogadoresService: JogadoresService) {}
 
   @Post()
-  async criarAtualizarJogador(@Body() criaJogadorDto: CriarJogadorDto) {
+  async criarAtualizarJogador(
+    @Body() criaJogadorDto: CriarJogadorDto) {
     await this.jogadoresService.criarAtualizarJogador(criaJogadorDto)
   }
 
   @Get()
-  async consultarJogadores(): Promise<Jogador[]> {
-    return this.jogadoresService.consultarTodosJogadores();
+  async consultarJogadores(
+    @Query('email') email : string): Promise<Jogador[] | Jogador> {
+      if (email) {
+        return await this.jogadoresService.consultarJogadorPeloEmail(email);
+      } else {
+        return await this.jogadoresService.consultarTodosJogadores();
+      }
   }
+
+  @Delete()
+  async deletarJogador(
+    @Query('email') email: string): Promise<void> {
+      this.jogadoresService.deletarJogador(email)
+    }
 
 }
--- src/jogadores/jogadores.service.ts
+++ src/jogadores/jogadores.service.ts
@@ -1,4 +1,4 @@
-import { Injectable, Logger } from '@nestjs/common'
+import { Injectable, Logger, NotFoundException } from '@nestjs/common'
 import { CriarJogadorDto } from './dtos/criar-jogador.dto'
 import { Jogador } from './interfaces/jogador.interface'
 import {v4 as uuid} from 'uuid'
@@ -15,18 +15,31 @@ export class JogadoresService {
     const { email } = criaJogadorDto;
 
     // se o jogador já existir no array retorna
-    const jogadorEncontrado = await this.jogadores.find(jogador => jogador.email === email)
+    const jogadorEncontrado = this.jogadores.find(jogador => jogador.email === email)
 
     if (jogadorEncontrado) {
-      await this.atualizar(jogadorEncontrado, criaJogadorDto);
+      this.atualizar(jogadorEncontrado, criaJogadorDto);
     } else {
-      await this.criar(criaJogadorDto)
+      this.criar(criaJogadorDto)
     }
 
   }
 
   async consultarTodosJogadores(): Promise<Jogador[]> {
     return await this.jogadores;
+  }
+
+  async deletarJogador(email): Promise<void> {
+    const jogadorEncontrado = this.jogadores.find(jogador => jogador.email === email)
+    this.jogadores = this.jogadores.filter(jogador => jogador.email !== jogadorEncontrado.email)
+  }
+
+  async consultarJogadorPeloEmail(email: string): Promise<Jogador> {
+    const jogadorEncontrado = this.jogadores.find(jogador => jogador.email === email)
+    if (!jogadorEncontrado) {
+      throw new NotFoundException(`Jogador com e-mail ${email} não encontrado`)
+    }
+    return jogadorEncontrado;
   }
 
   private criar(criaJogadorDto: CriarJogadorDto): void {
